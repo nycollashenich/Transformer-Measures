8c08f0bf0528e34d4f03b6ddaae055b20850713b
Nycollas <nhhenich@gmail.com>
2024-12-23 22:59:59 -0300

    metrico system

--- system/system_imperial.rb
+++ system/system_imperial.rb
@@ -1,17 +1,17 @@
-require_relative 'C:/transformer_measures/teste/unit_to_meters.rb'
+require_relative 'C:/transformer_measures/universal_convert/unit_to_meters.rb'
 
 class ImperialMeasure
 attr_reader :value, :unit
-def initialize(value, unit)
-    @value = value
-    @unit = unit.downcase
+    def initialize(value, unit)
+        @value = value
+        @unit = unit.downcase
+    end
+
+    def to_meters
+        Convert.unit_to_meters(@value, @unit)
+    end
+
 end
 
-def to_meters
-    Convert.unit_to_meters(@value, @unit)
-end
-
-end
-
-meters = Convert.unit_to_meters(10, 'millimeter')   
+meters = Convert.unit_to_meters(2, 'mile')   
 p "#{meters}"
