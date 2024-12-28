7e01b81cbc5d47d0a26b68e76c6dae0385dccaa8
Nycollas <nhhenich@gmail.com>
2024-12-27 22:13:10 -0300

    new arquiteture

--- lib/conversion/conversion_length.rb
+++ lib/conversion/conversion_length.rb
@@ -1,29 +1,20 @@
-require_relative './conversion_base.rb'
+module LengthConversion
+    FACTORS = {
+    "millimeter" => 0.001,
+    "centimeter" => 0.01,
+    "meter" => 1,
+    "kilometer" => 1000,
+    "inch" => 0.0254,
+    "foot" => 0.3048,
+    "yard" => 0.9144,
+    "mile" => 1609.34
+  }
 
-class ConversionLength
-
-  include Conversion # base_convertion.rb
-
-    def initialize(value, origin_unit, destination_unit)
-      @value = value
-      @origin_unit = origin_unit
-      @destination_unit = destination_unit
-    end
-
-    def convert
-      if FACTORS_LENGTH.key?(@origin_unit) && FACTORS_LENGTH.key?(@destination_unit)
-        convert_length
-      else
-        raise 'Unidades incompatíveis'
-      end 
-    end
-    
-    private
-    def convert_length
-      origin_factor = FACTORS_LENGTH[@origin_unit]
-      destination_factor = FACTORS_LENGTH[@destination_unit]
-      value_in_meters = @value * origin_factor
-      value_in_meters / destination_factor
-    end
+  def self.convert(value, origin_unit, destination_unit)
+    value_in_meters = value * FACTORS[origin_unit]
+    value_in_meters / FACTORS[destination_unit]
+  end
 
 end
+
+p LengthConversion.convert(1, 'meter', 'millimeter')
